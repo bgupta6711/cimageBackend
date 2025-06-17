915b12ac2562a6b2d484e751600544798e21ce84
Bhavesh Kumar <bgupta6711@gmail.com>
2025-06-17 09:20:18 +0530

    Django worshop

---
+++ Django workshop/DLFMALL/mallusers/admin.py
@@ -0,0 +1,8 @@
+from .models import *
+from django.contrib import admin
+
+admin.site.register(userDetails)
+admin.site.register(membershipDetails)
+admin.site.register(eventDetails)
+admin.site.register(registrationDetails)
+
